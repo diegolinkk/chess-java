095789f72a2a846c6f0a35ccbe31035df601f180
Diego Oliveira da Silva <diego-oliveira.silva@serpro.gov.br>
2024-04-09 14:52:53 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,10 +25,19 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		
+//		essa de cima da erro, provável erro na conversão em posição
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		
+	}	
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(7,1));
-		board.placePiece(new King(board, Color.BLACK), new Position(1,4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+		//
+		placeNewPiece('b', 6, new Rook(board,Color.WHITE));
+		placeNewPiece('e', 4, new Rook(board,Color.WHITE)); 
+		placeNewPiece('a',1,new King(board, Color.WHITE));
+
 	}
 	
 }
