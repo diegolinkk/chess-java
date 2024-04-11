555aeabc960c70bc4861f58b35992d66b8f5c131
Diego Oliveira da Silva <diego-oliveira.silva@serpro.gov.br>
2024-04-11 16:57:51 -0300

    Implementing possible moves of King

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -85,6 +85,7 @@ public class ChessMatch {
         placeNewPiece('e', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
+        placeNewPiece('g', 1, new King(board, Color.WHITE));
 	}
 	
 }
