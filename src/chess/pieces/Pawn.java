0f436cddff015aced8bd0a2bde7fecfdc3592f5a
Diego Oliveira da Silva <diego-oliveira.silva@serpro.gov.br>
2024-04-12 15:25:39 -0300

    Queen

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -46,8 +46,8 @@ public class Pawn extends ChessPiece{
 			}
 			
 			p.setValues(position.getRow() +2, position.getColumn());
-			Position p2 = new Position(position.getRow() -1 ,position.getColumn());
-			
+			Position p2 = new Position(position.getRow() +1 ,position.getColumn());
+			//getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && 	getBoard().positionExists(p2)&& !getBoard().thereIsAPiece(p2) && getMoveCount() == 0
 			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)&& !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
